cbafc23c1bcc51201a43d934175c150317593391
Prakharsen <prakhar.itech@gmail.com>
2023-05-31 17:01:26 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  currentItem = 'Television';
+  persons = [
+
+    {
+        "id":1,
+        "name":"Akshit",
+        "city":"Moradabad"
+    },
+    
+    {
+        "id":2,
+        "name":"Nikita",
+        "city":"Lucknow"
+    },
+    
+    {
+        "id":3,
+        "name":"Deeksha",
+        "city":"Noida"
+    },
+    
+    {
+        "id":4,
+        "name":"Ritesh",
+        "city":"Delhi"
+    },
+    
+    {
+        "id":5,
+        "name":"Somya",
+        "city":"Gurugram"
+    },
+    
+    {
+        "id":6,
+        "name":"Eshika",
+        "city":"Mumbai"
+    },
+    {
+        "id":7,
+        "name":"Kalpana",
+        "city":"Rampur"
+    },
+    
+    {
+        "id":8,
+        "name":"Parul",
+        "city":"Chandigarh"
+    },
+    
+    {
+        "id":9,
+        "name":"Himani",
+        "city":"Dehradun"
+    }
+]
 }
